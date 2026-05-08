a16fa1b3e8de08970b3817aa488ce3eeb885080c
wezzybug <wezzybug@users.noreply.github.com>
2026-05-08 13:15:46 +0200

    chore: cleanup temp files

--- stocks_replacement.js
+++
@@ -1,4 +0,0 @@
-// ===== STOCKS =====
-function buyStock(i,qty){SERVER.action('buy_stock',{index:i,qty:qty}).then(r=>{if(r&&r.success){gold=r.gold;update();renderStocks();}});}
-function sellStock(i,qty){SERVER.action('sell_stock',{index:i,qty:qty}).then(r=>{if(r&&r.success){gold=r.gold;update();renderStocks();}});}
-function sellAllStock(i){SERVER.action('sell_stock',{index:i,qty:stockState[i].shares}).then(r=>{if(r&&r.success){gold=r.gold;update();renderStocks();}});}
